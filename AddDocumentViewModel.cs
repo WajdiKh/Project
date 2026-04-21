bbf16fc90790d6ab3c58003ae7a7f622facb9dcb
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-04-21 16:39:52 +0200

    Create AddDocumentViewModel.cs

---
+++ AddDocumentViewModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace BacaratWeb.ViewModel.Transfert
+{
+    public class AddDocumentViewModel
+    {
+        [Required(ErrorMessage = "Le fichier est obligatoire.")]
+        public IFormFile File { get; set; }
+
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Le destinataire est obligatoire.")]
+        [EmailAddress(ErrorMessage = "Email invalide.")]
+        public string RecipientEmail { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+    }
+}
