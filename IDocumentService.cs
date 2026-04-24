dc4ec0c5b0096c3d3a8b5541fa3d91b89c6bab6d
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-04-24 14:22:27 +0200

    Create IDocumentService.cs

---
+++ IDocumentService.cs
@@ -0,0 +1,28 @@
+using BacaratWeb.Entities.Transfert;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BacaratWeb.Services.Transfert.Services.Interfaces
+{
+    public interface IDocumentService
+    {
+        Task<bool> AddDocumentAsync(
+            Document document,
+            DocumentShare documentShare,
+            CancellationToken token = default);
+
+        Task<IEnumerable<Document>> GetMyDocumentsAsync(
+            int ownerId,
+            CancellationToken token = default);
+
+        Task<IEnumerable<DocumentShare>> GetSharedWithMeDocumentsAsync(
+            int userId,
+            string email,
+            CancellationToken token = default);
+
+        Task<StatutDocument> GetStatutDocumentByCodeAsync(
+            string code,
+            CancellationToken token = default);
+    }
+}
