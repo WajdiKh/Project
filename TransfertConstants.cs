2ae2880ccdfb1b716cc795764ffd98473f11ccd4
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-05-11 13:59:39 +0200

    Create TransfertConstants.cs

---
+++ TransfertConstants.cs
@@ -0,0 +1,9 @@
+namespace BacaratWeb.Areas.Transfert.Helpers
+{
+    public static class TransfertConstants
+    {
+        public const string AddNewDocumentPopupId = "AddNewDocumentPopup";
+        public const string AddNewDocumentFormId = "AddNewDocumentForm";
+        public const string AddNewDocumentFormValidationGroupName = "AddNewDocumentFormValidationGroup";
+    }
+}
