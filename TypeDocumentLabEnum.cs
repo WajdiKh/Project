acfc9083b6887aa00f6328daac5f8047682ad113
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-06-18 17:14:02 +0200

    Create TypeDocumentLabEnum.cs

---
+++ TypeDocumentLabEnum.cs
@@ -0,0 +1,24 @@
+namespace BacaratWeb.Shared
+{
+    public enum TypeDocumentLabEnum
+    {
+        TA1,
+        TA2,
+        TA3,
+        TA4,
+        TA5,
+        TA6,
+        TA7,
+        TA8,
+        TA9,
+        TA11,
+        TA12,
+        TA13,
+        TA14,
+        TA15,
+        TA16,
+        TA18,
+        TA19,
+        TA17
+    }
+}
