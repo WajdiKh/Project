539a13a417cf745a20d618a2a8aa77a17ae3a904
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-06-29 17:17:09 +0200

    Update DocumentShare.sql

--- DocumentShare.sql
+++ DocumentShare.sql
@@ -9,31 +9,67 @@ CREATE TABLE [Transfert].[DocumentShare]
     [Email]            NVARCHAR(320) NOT NULL,
     [SharedDate]       DATETIMEOFFSET(7) NOT NULL,
     [ExpiryDate]       DATETIMEOFFSET(7) NOT NULL,
-
+    [CreateurId] INT NULL,
     CONSTRAINT [PK_DocumentShare]
         PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 GO
 
-ALTER TABLE [Transfert].[DocumentShare]
-ADD CONSTRAINT [DF_DocumentShare_SharedDate]
-DEFAULT (SYSDATETIMEOFFSET()) FOR [SharedDate];
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = 'IX_DocumentShare_DocumentId'
+      AND object_id = OBJECT_ID('[Transfert].[DocumentShare]')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_DocumentShare_DocumentId]
+    ON [Transfert].[DocumentShare] ([DocumentId])
+    INCLUDE ([Id], [Email], [SharedWithUserId], [CreateurId], [SharedDate], [ExpiryDate]);
+END
 GO
 
-ALTER TABLE [Transfert].[DocumentShare]
-ADD CONSTRAINT [FK_DocumentShare_Document_DocumentId]
-FOREIGN KEY ([DocumentId]) REFERENCES [Transfert].[Document]([Id]);
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = 'IX_DocumentShare_SharedWithUserId_ExpiryDate'
+      AND object_id = OBJECT_ID('[Transfert].[DocumentShare]')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_DocumentShare_SharedWithUserId_ExpiryDate]
+    ON [Transfert].[DocumentShare] ([SharedWithUserId], [ExpiryDate])
+    INCLUDE ([Id], [DocumentId], [Email], [SharedDate]);
+END
 GO
 
-ALTER TABLE [Transfert].[DocumentShare]
-ADD CONSTRAINT [FK_DocumentShare_Utilisateur_SharedWithUserId]
-FOREIGN KEY ([SharedWithUserId]) REFERENCES [Commun].[Utilisateur]([Id]);
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = 'IX_DocumentShare_Email_ExpiryDate'
+      AND object_id = OBJECT_ID('[Transfert].[DocumentShare]')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_DocumentShare_Email_ExpiryDate]
+    ON [Transfert].[DocumentShare] ([Email], [ExpiryDate])
+    INCLUDE ([Id], [DocumentId], [SharedWithUserId], [SharedDate]);
+END
 GO
 
-CREATE NONCLUSTERED INDEX [IX_DocumentShare_DocumentId]
-ON [Transfert].[DocumentShare]([DocumentId]);
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = 'IX_DocumentShare_DocumentId_SharedWithUserId_Email'
+      AND object_id = OBJECT_ID('[Transfert].[DocumentShare]')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_DocumentShare_DocumentId_SharedWithUserId_Email]
+    ON [Transfert].[DocumentShare] ([DocumentId], [SharedWithUserId], [Email])
+    INCLUDE ([Id], [SharedDate], [ExpiryDate]);
+END
 GO
 
-CREATE NONCLUSTERED INDEX [IX_DocumentShare_SharedWithUserId]
-ON [Transfert].[DocumentShare]([SharedWithUserId]);
+IF NOT EXISTS (
+    SELECT 1 FROM sys.indexes
+    WHERE name = 'IX_DocumentShare_CreateurId'
+      AND object_id = OBJECT_ID('[Transfert].[DocumentShare]')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX [IX_DocumentShare_CreateurId]
+    ON [Transfert].[DocumentShare] ([CreateurId]);
+END
 GO
