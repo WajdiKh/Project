2fbeaf559768f999c4abf9b03c91cb305dd698fe
WajdiKh <63234301+WajdiKh@users.noreply.github.com>
2026-06-29 17:20:46 +0200

    Update DocumentShare.sql

--- DocumentShare.sql
+++ DocumentShare.sql
@@ -9,7 +9,9 @@ CREATE TABLE [Transfert].[DocumentShare]
     [Email]            NVARCHAR(320) NOT NULL,
     [SharedDate]       DATETIMEOFFSET(7) NOT NULL,
     [ExpiryDate]       DATETIMEOFFSET(7) NOT NULL,
-    [CreateurId] INT NULL,
+    [LastDownloadDate] DATETIMEOFFSET(7) NULL,
+    [IsActive]         BIT NOT NULL CONSTRAINT [DF_DocumentShare_IsActive] DEFAULT (1),
+    [CreateurId]       INT NULL,
     CONSTRAINT [PK_DocumentShare]
         PRIMARY KEY CLUSTERED ([Id] ASC)
 );
